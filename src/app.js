9a268cebd7875581e8d720961bb94a05b85f145a
Diego Casermeiro Gálvez <122561493+diegokser@users.noreply.github.com>
2023-04-17 19:03:10 +0000

    cambios en archivo app.js index.html

--- src/app.js
+++ src/app.js
@@ -5,7 +5,33 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const getRandomElement = myArray => {
+    let randomElement = myArray[Math.floor(Math.random() * myArray.length)];
+
+    return randomElement;
+  };
+
+  let stringGenerator =
+    getRandomElement(who) +
+    " " +
+    getRandomElement(action) +
+    " " +
+    getRandomElement(what) +
+    " " +
+    getRandomElement(when);
+
+  document.querySelector("#excuse").innerHTML = stringGenerator;
 };
